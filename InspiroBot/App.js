6b01d204c7aa9b736d626b19100ff6d8c8519668
Ginger Barham <gingermostly@gmail.com>
2019-05-04 15:59:43 -0700

    Add fetch request to get image from InspiroBot api

--- InspiroBot/App.js
+++ InspiroBot/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
-import { Image, View } from 'react-native';
+import { Button, Image, View } from 'react-native';
+
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      getImage: ''
+    }
+  }
+  componentDidMount() {
+    fetch('https://inspirobot.me/api?generate=true')
+      .then((res) => {
+        return res.text()
+    })
+      .then((item) => {
+       this.setState({
+         getImage: item
+       })
+    })
+  }
   render() {
     let pic = {
-      uri: 'https://generated.inspirobot.me/a/p9eZk8yqJV.jpg'
+      uri: this.state.getImage
     }
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Image source={pic} style={{width: 300, height: 300}} />
+        <Button title="Generate Inspiration"/>
       </View>
     );
   }
