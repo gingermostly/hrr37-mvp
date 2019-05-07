6c01d8658d7d28f5a4e0804c20e7e7c35d90087a
Ginger Barham <gingermostly@gmail.com>
2019-05-06 21:27:32 -0700

    Add share functionality

--- InspiroBot/App.js
+++ InspiroBot/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ActionSheetIOS, TouchableOpacity, Text, Image, View } from 'react-native';
+import { ActionSheetIOS, TouchableWithoutFeedback, TouchableOpacity, Text, Image, View, Share } from 'react-native';
 import CameraRoll from '@react-native-community/cameraroll';
 import * as RNFS from 'react-native-fs';
 
@@ -9,10 +9,11 @@ export default class App extends Component {
     super(props);
     this.state = {
       getImage: 'https://i.imgur.com/2h816KP.jpg',
-
+      share: ''
     }
     this.handlePress = this.handlePress.bind(this);
     this.handleLongPress = this.handleLongPress.bind(this);
+    this.imageShare = this.imageShare.bind(this);
   }
   handlePress() {
       fetch('https://inspirobot.me/api?generate=true')
@@ -21,7 +22,8 @@ export default class App extends Component {
       })
         .then((item) => {
          this.setState({
-        getImage: item
+        getImage: item,
+        share: 'Share this photo'
     })
   })
 }
@@ -32,6 +34,13 @@ handleLongPress() {
     }
   })
 }
+imageShare() {
+  alert('clicked')
+  Share.share({
+    url: this.state.getImage,
+    message: '#Inspired'
+  })
+}
   render() {
     let pic = {
       uri: this.state.getImage
@@ -39,10 +48,15 @@ handleLongPress() {
     return (
       <View style={{ flex: 1, flexirection: "column", justifyContent: "center", alignItems: "center" }}>
         <Text style={{fontSize: 30, fontWeight: 'bold', marginBottom: 10}}>InspiroBot Mobile</Text>
-        <TouchableOpacity onPress={this.handlePress} onLongPress={this.handleLongPress}>
-        <Image source={pic} style={{width: 300, height: 400, resizeMode: 'contain'}} />
-        </TouchableOpacity>
-          <Text style={{fontSize: 20, marginBottom: 10}}>Tap image to generate inspiration</Text>
+        <TouchableWithoutFeedback onPress={this.handlePress} onLongPress={this.handleLongPress}>
+        <Image source={pic} style={{width: 300, height: 300, marginBottom: 10, resizeMode: 'contain'}} />
+        </TouchableWithoutFeedback>
+        <TouchableOpacity onPress={this.imageShare}>
+          <Text style={{fontSize: 16, marginTop: 5, marginBottom: 10}}>{this.state.share}
+            </Text>
+            </TouchableOpacity>
+          <Text style={{fontSize: 20, marginBottom: 10}}>Tap image to generate inspiration
+            </Text>
       </View>
     );
   }
